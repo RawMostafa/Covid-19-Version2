55cc29b08a06e1d8c4105b5d93b07251f18a690a
RawMostafa <rawanmostafa.mn@gmail.com>
2021-09-15 14:10:22 +0200

    first commit Version

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// Nice scroll
+// $(function() {  
+//     $("body").niceScroll({scrollspeed: 120})
+// }); 
+
+
+// <!-- textillate.js  -->
+$(function () {
+	$('.tlt').textillate({ 
+    in: { effect: 'rollIn', delay:10, },
+    out: { effect: 'rollout', delay:10, },
+    loop: true,
+  });
+})
+
+
